a5e64a5f868c52ee4b0365df6a7cf6a32fea8caa
sisi_m <yuansisi@supersisideshiguangji.local>
2018-10-25 18:53:08 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<style src="../../assets/css/global.css"></style>
+<style module>
+	.app-header[data-role="app-header"]{
+		& .title{
+			color: #00f;
+		}
+	}
+</style>
+
+<template>
+	<div id="app">
+		<app-header :class="$style['app-header']"></app-header>
+	</div>
+</template>
+
+<script>
+import header from './components/header/header'
+export default {
+	name: 'app',
+	components: {
+		"app-header": header
+	}
+}
+</script>
---
+++ src/components/header/header.vue
@@ -0,0 +1,12 @@
+<style module src="./header.css"></style>
+<template name="header" src="./header.html"></template>
+
+<script>
+export default {
+  name: '',
+  props: {
+    
+  }
+}
+</script>
+
